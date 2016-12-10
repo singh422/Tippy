43cca2338c451375fac71db793250098f34ae067
Avinash <@purdue.edu>
2016-12-10 16:12:56 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Avinash Singh on 10/12/16.
+//  Copyright © 2016 Avinash Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
